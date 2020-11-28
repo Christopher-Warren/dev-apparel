f1afe246c8b6f7fcd47e7116af9976b37743cfe1
Christopher Warren <chrisalmith@gmail.com>
2020-11-27 22:24:18 -0500

    Update [category].js

--- pages/shop/categories/[category].js
+++ pages/shop/categories/[category].js
@@ -1,16 +1,14 @@
 import { useRouter } from "next/router";
-import Header from "../../../components/header";
 
 const Category = () => {
   const router = useRouter();
-  const { id, comment } = router.query;
+  const { category } = router.query;
 
   return (
-    <>
-      <Header />
-      <h1>Post: {id}</h1>
-      <h1>Comment: {comment}</h1>
-    </>
+    <div className="mt-5">
+      <h1>Post: {}</h1>
+      <h1>Comment: {category}</h1>
+    </div>
   );
 };
 
